68281e775b7157b566c10e51b2776bca1646ad39
sugurunnba <sugurunnba7@gmail.com>
2021-03-26 15:10:54 +0900

    first-commit

---
+++ JavaWcp2/src/Ready02.java
@@ -0,0 +1,13 @@
+
+public class Ready02 {
+
+//	public static void main(String[] args) {
+//		// TODO 自動生成されたメソッド・スタブ
+//		System.out.println("Hello World!");
+//	}
+
+	public static void main(String[] args) {
+		System.out.println("こんにちわ！");
+	}
+
+}
