557727206fb3f46aa168b404e87febcc7ddaf699
sugurunnba <sugurunnba7@gmail.com>
2021-03-29 19:08:42 +0900

    lesson-5

---
+++ JavaWcp2/src/Chapter05.java
@@ -0,0 +1,24 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		long a = 100000000000L;
+		System.out.println(a);
+		double b = 0.1;
+		System.out.println(b);
+		boolean c = true;
+		System.out.println(c);
+
+		Integer i = 100;
+		System.out.println(i);
+		Float f = 1.4142f;
+		System.out.println(f);
+		Character e = 'う';
+		System.out.println(e);
+
+		int[] array = new int[] {10, 20, 50, 100};
+ 		System.out.println(array[3]);
+	}
+
+}
