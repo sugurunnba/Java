e1fb4787e9ee50b92e5490bce6eef568e3f3366d
sugurunnba <sugurunnba7@gmail.com>
2021-04-07 15:09:54 +0900

    lesson-9

---
+++ JavaWcp2/src/Human01.java
@@ -0,0 +1,17 @@
+
+public class Human01 {
+	public String name;
+	public int age;
+	
+	public Human01() {
+		name = "山田";
+		age = 20;
+	}
+	
+	public Human01(String name, int age) {
+		// TODO 自動生成されたメソッド・スタブ
+		this.name = name;
+		this.age = age;
+	}
+
+}
