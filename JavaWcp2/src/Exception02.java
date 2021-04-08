15d5172f46e637326d9200286f0c66d079cf06bf
sugurunnba <sugurunnba7@gmail.com>
2021-04-08 14:59:03 +0900

    lesson-10

---
+++ JavaWcp2/src/Exception02.java
@@ -0,0 +1,17 @@
+
+public class Exception02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		try {
+			System.out.println("100 ÷ 0 は?");
+			int result = 100 / 0;
+			System.out.println("計算結果" + result);
+		} catch (ArithmeticException e) {
+			System.out.println("例外が発生");
+		} finally {
+			System.out.println("プログラム終了");
+		}
+	}
+
+}
---
+++ JavaWcp2/src/exception/Exception01.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class Exception01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("100 ÷ 0 は?");
+		int result = 100 / 0;
+		System.out.println("計算機 + result");
+		System.out.println("プログラム終了");
+	}
+
+}
